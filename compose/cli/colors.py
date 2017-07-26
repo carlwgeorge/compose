567d0a0ccb5ab4a338d812eb75c20817f85d38b7
Carl George <carl@george.computer>
2017-07-26 16:54:40 -0500

    conditionally import colorama

--- compose/cli/colors.py
+++ compose/cli/colors.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-import colorama
+import platform
 
 NAMES = [
     'grey',
@@ -33,7 +33,9 @@ def make_color_fn(code):
     return lambda s: ansi_color(code, s)
 
 
-colorama.init(strip=False)
+if platform.system() == 'Windows':
+    import colorama
+    colorama.init(strip=False)
 for (name, code) in get_pairs():
     globals()[name] = make_color_fn(code)
 
